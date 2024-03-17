a17228079b84b1e61b06e92389a6f1c88d42ad12
asifalam515 <asibulalam515@gmail.com>
2024-03-17 11:31:15 +0600

    cors added

--- task_master_drf/settings.py
+++ task_master_drf/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'category',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'task_master_drf.urls'
 
